9976cb2f73b22e78eaaf8c7e5d31b5f58434f689
Elizabeth5335 <51746623+Elizabeth5335@users.noreply.github.com>
2023-07-13 15:48:29 +0300

    finished main functionality

--- exchange/src/app/convert/convert.component.ts
+++ exchange/src/app/convert/convert.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-import { Observable, of, throwError } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { catchError, map, switchMap } from 'rxjs/operators';
 import { FormControl } from '@angular/forms';
+import { __values } from 'tslib';
 
   
 
@@ -22,118 +21,48 @@ export class ConvertComponent {
   curr2 = new FormControl('UAH');
   val1 = new FormControl('1');
   val2 = new FormControl('');
-  
-
+  defVal = '0';
   rate = '';
 
 
-  getRate(curr1: string | null, curr2: string | null): string {
+  async getRate(curr1: string | null, curr2: string) {
     const url = `https://v6.exchangerate-api.com/v6/da14864407c36d688f8b4427/latest/${curr1}`;
   
-    this.http.get<any>(url).subscribe(
-      (response) => {
-        switch (curr1) {
-          case 'USD':
-            switch (curr2) {
-              case 'UAH':
-                this.rate = response.conversion_rates.UAH;
-                break;
-              case 'EUR':
-                this.rate = response.conversion_rates.EUR;
-                break;
-              case 'PLN':
-                this.rate = response.conversion_rates.PLN;
-                break;
-              default:
-                this.rate = '1';
-                break;
-            }
-            break;
-          case 'EUR':
-            switch (curr2) {
-              case 'UAH':
-                this.rate = response.conversion_rates.UAH;
-                break;
-              case 'USD':
-                this.rate = response.conversion_rates.USD;
-                break;
-              case 'PLN':
-                this.rate = response.conversion_rates.PLN;
-                break;
-              default:
-                this.rate = '1';
-                break;
-            }
-            break;
-          case 'PLN':
-            switch (curr2) {
-              case 'UAH':
-                this.rate = response.conversion_rates.UAH;
-                break;
-              case 'EUR':
-                this.rate = response.conversion_rates.EUR;
-                break;
-              case 'USD':
-                this.rate = response.conversion_rates.USD;
-                break;
-              default:
-                this.rate = '1';
-                break;
-            }
-            break;
-          case 'UAH': 
-            switch (curr2) {
-              case 'USD':
-                this.rate = response.conversion_rates.USD;
-                break;
-              case 'EUR':
-                this.rate = response.conversion_rates.EUR;
-                break;
-              case 'PLN':
-                this.rate = response.conversion_rates.PLN;
-                break;
-              default:
-                this.rate = '1';
-                break;
-            }
-            break;
-          default:
-            this.rate = response.conversion_rates.USD;
-            break;
-        }
-      },
-      (error) => {
-        console.error(error);
+    try {
+      const response = await this.http.get<any>(url).toPromise();
+      if (response.conversion_rates.hasOwnProperty(curr2)) {
+        this.rate = response.conversion_rates[curr2];
+        console.log('Updated rate:', this.rate);
       }
-    );
-
-    return this.rate;
-    
+    } catch (error) {
+      console.error(error);
+    }
   }
-  
+
+
   convert(value: any, curr1: any, curr2: any, rate: string): number {
     return parseFloat(value)*parseFloat(rate);
   }
-    
 
-  onCurrencyChange(valToChange : any, value: any, curr1: any, curr2: any): void {
-    this.rate = this.getRate(curr1, curr2);
-    if (value && curr1 && curr2) {
-      const convertedValue = this.convert(value.value, curr1.value, curr2.value, this.rate);
-      valToChange.setValue(convertedValue.toString());
+
+  async onCurrencyChange(valueToChange: any, value: any, curr1: any, curr2: any): Promise<void> {
+    if(curr2.value){
+      await this.getRate(curr1.value, curr2.value)
     }
+    const convertedValue = this.convert(value.value, curr1.value, curr2.value, this.rate).toString();
+    valueToChange.setValue(convertedValue);
   }
 
-  onInputChange(valToChange : any, value: any, curr1: any, curr2: any): void {
-    if (value && curr1 && curr2) {
-      const convertedValue = this.convert(value.value, curr1.value, curr2.value, this.rate);
-      valToChange.setValue(convertedValue.toString());
+
+  async changeDefVal(){
+    if(this.curr2.value){
+      await this.getRate(this.curr1.value,this.curr2.value); 
     }
+    this.defVal = this.rate;
   }
 
-  
-  ngOnInit(){   
-    // this.onCurrencyChange(this.val2, '1', this.curr1, this.curr2.value);
+
+  async ngOnInit(){   
     this.http.get<any>('https://v6.exchangerate-api.com/v6/da14864407c36d688f8b4427/latest/USD').subscribe(
       (response) => {
         this.val2 = new FormControl(response.conversion_rates.UAH);
@@ -142,7 +71,10 @@ export class ConvertComponent {
         console.error(error);
       }
     );
-    this.rate = this.getRate(this.curr1.value,this.curr2.value); 
+    if(this.curr2.value){
+      await this.getRate(this.curr1.value,this.curr2.value); 
+    }
+    this.defVal = this.convert('1', this.curr1.value, this.curr2.value, this.rate).toString();
   }
   
 }
